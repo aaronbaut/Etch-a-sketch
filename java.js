066a8f6a85f80da1b065a87912562a26d9cc57c9
Aaron <aaronbaut95@gmail.com>
2022-06-27 21:37:32 -0700

    Fix syntax to allow mouseover listener to work
    Adds mouseover event which performs an action for each
    content div

--- java.js
+++ java.js
@@ -5,5 +5,11 @@ for(i=0;i<256;i++) {
     const content = document.createElement('div')
     content.classList.add('content')
     container.appendChild(content)
-    console.log(content)
     }
+
+const contents = document.querySelectorAll('.content');
+contents.forEach((content) => {
+    content.addEventListener('mouseover', () => {
+        console.log('hi')
+    });
+});
